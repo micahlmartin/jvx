f3617b3046e6a1e765c8ec879fef4426cd1cdc93
Micah Martin <micahlmartin@gmail.com>
2025-03-11 17:13:03 -0400

    feat(layout): update main layout colors
    - Apply new theme colors to main layout components
    
    - Ensure consistent background colors across the application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,7 +78,7 @@ function HomeContent() {
   return (
     <ReactFlowProvider>
       <div className="h-[100dvh] w-full overflow-hidden bg-background dark:bg-background-dark relative">
-        <div className="h-[38px] w-full border-b border-node-border dark:border-node-border-dark flex items-center bg-background dark:bg-background-dark">
+        <div className="h-[38px] w-full border-b border-node-border dark:border-node-border-dark flex items-center bg-toolbar-bg dark:bg-toolbar-bg-dark">
           <MenuBar
             documents={documents}
             activeDocument={activeDocument}
@@ -88,7 +88,7 @@ function HomeContent() {
         </div>
         <div className="h-[calc(100%-38px)] w-full relative">
           <div 
-            className={`absolute top-0 left-0 h-full w-[400px] border-r border-node-border dark:border-node-border-dark bg-background dark:bg-background-dark transition-transform duration-300 ease-in-out z-10 ${
+            className={`absolute top-0 left-0 h-full w-[400px] border-r border-node-border dark:border-node-border-dark bg-sidebar-bg dark:bg-sidebar-bg-dark transition-transform duration-300 ease-in-out z-10 ${
               isSidebarVisible ? 'translate-x-0' : '-translate-x-full'
             }`}
           >
